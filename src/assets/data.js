1e1cb807d57b72eddc253d665dbfcad6cc8a2c66
Elif Kevser Tölek <152923266+Elifkvsr@users.noreply.github.com>
2025-04-17 00:12:09 +0300

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question: "Inter için hangi cihaz gereklidir?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Hangi kıtada en fazla ülke var?",
+        option1: "Asya",
+        option2: "Avrupa",
+        option3: " Kuzey Amerika",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "Junck Email ayrıca şu şekilde de adlandırılır",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archived",
+        option4: "Bin",
+        ans: 1,
+    },
+    {
+        question: "Bir bilgisayar aşağıdakilerden birine sahip değilse ÖN YÜKLEME yapamaz?",
+        option1: "Uygulama Yazılımı",
+        option2: "internet",
+        option3: "İşletim Sistemi",
+        option4: "Fare",
+        ans: 3,
+    },
+    {
+        question: "Web sitesinin ilk sayfasına ne ad verilir?",
+        option1: "Index Page",
+        option2: "Homepage",
+        option3: "İşletim Sitemap",
+        option4: "Pen Drive",
+        ans: 2,
+    },
+
+];
